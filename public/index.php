4c13d540fbfe0659bbffeb46ec5dfa302c4f635a
Ayman Elkassas <aymanelkassas88@gmail.com>
2020-04-01 05:38:32 +0200

    wrong open close principle

--- public/index.php
+++ public/index.php
@@ -2,5 +2,11 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-$match=new \Classes\Match();
+$players=[];
+
+for($index=1;$index<12;$index++){
+	array_push($players,new Classes\Player());
+}
+
+$match=new Classes\Match($players);
 $match->start();
